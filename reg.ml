31fdaaaff71cac6edb25ea749ac394bf320355da
Tran Sang <nissassin17@gmail.com>
2016-11-15 14:51:34 +0900

    mistakenly remove heap pointer register

--- reg.ml
+++ reg.ml
@@ -1,7 +1,7 @@
 type t = string
 let reg_no i = Printf.sprintf "%%r%d" i
 let freg_no i = Printf.sprintf "%%fr%d" i
-let regs = Array.init 29 reg_no
+let regs = Array.init 28 reg_no
 let fregs = Array.init 31 freg_no
 let allregs = Array.to_list regs
 let allfregs = Array.to_list fregs
@@ -11,4 +11,5 @@ let reg_dump = "%rd" (*r31*)
 let freg_dump = "%frd" (*fr31*)
 let reg_ret = reg_no 0
 let freg_ret = freg_no 0
+let reg_hp = "%rhp" (*r28*)
 (* let reg_ra = "%eax" (* return address *) *)
