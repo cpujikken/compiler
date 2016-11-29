6118c2ffadec4a7e2b4aee151375f120b1f8d6fa
Tran Sang <tranvansangk41@gmail.com>
2016-11-29 15:32:40 +0900

    add global.s

---
+++ globals.s
@@ -0,0 +1,69 @@
+.data
+gobjects:
+	.space	240
+gsize:
+	.space	8
+gdbg:
+	.space	4
+gscreen:
+	.space	24
+gvp:
+	.space	24
+gview:
+	.space	24
+glight:
+	.space	24
+gcos_v:
+	.space	16
+gsin_v:
+	.space	16
+gbeam:
+	.space	8
+gand_net:
+	.space	200
+gor_net:
+	.space	4
+gtemp:
+	.space	112
+gcs_temp:
+	.space	128
+gsolver_dist:
+	.space	8
+gvscan:
+	.space	24
+gintsec_rectside:
+	.space	4
+gtmin:
+	.space	8
+gcrashed_point:
+	.space	24
+gcrashed_object:
+	.space	4
+gend_flag:
+	.space	4
+gviewpoint:
+	.space	24
+gnvector:
+	.space	24
+grgb:
+	.space	24
+gtexture_color:
+	.space	24
+gsolver_w_vec:
+	.space	24
+gchkinside_p:
+	.space	24
+gisoutside_q:
+	.space	24
+gnvector_w:
+	.space	24
+gscan_d:
+	.space	8
+gscan_offset:
+	.space	8
+gscan_sscany:
+	.space	8
+gscan_met1:
+	.space	8
+gwscan:
+	.space	24
