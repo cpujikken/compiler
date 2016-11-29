92813a35d3de08b829fe52ca6521d368d52a2e45
Tran Sang <tranvansangk41@gmail.com>
2016-11-29 15:38:01 +0900

    change .space to .SPACE in global.s

--- globals.s
+++ globals.s
@@ -1,69 +1,69 @@
 .data
 gobjects:
-	.space	240
+	.SPACE	240
 gsize:
-	.space	8
+	.SPACE	8
 gdbg:
-	.space	4
+	.SPACE	4
 gscreen:
-	.space	24
+	.SPACE	24
 gvp:
-	.space	24
+	.SPACE	24
 gview:
-	.space	24
+	.SPACE	24
 glight:
-	.space	24
+	.SPACE	24
 gcos_v:
-	.space	16
+	.SPACE	16
 gsin_v:
-	.space	16
+	.SPACE	16
 gbeam:
-	.space	8
+	.SPACE	8
 gand_net:
-	.space	200
+	.SPACE	200
 gor_net:
-	.space	4
+	.SPACE	4
 gtemp:
-	.space	112
+	.SPACE	112
 gcs_temp:
-	.space	128
+	.SPACE	128
 gsolver_dist:
-	.space	8
+	.SPACE	8
 gvscan:
-	.space	24
+	.SPACE	24
 gintsec_rectside:
-	.space	4
+	.SPACE	4
 gtmin:
-	.space	8
+	.SPACE	8
 gcrashed_point:
-	.space	24
+	.SPACE	24
 gcrashed_object:
-	.space	4
+	.SPACE	4
 gend_flag:
-	.space	4
+	.SPACE	4
 gviewpoint:
-	.space	24
+	.SPACE	24
 gnvector:
-	.space	24
+	.SPACE	24
 grgb:
-	.space	24
+	.SPACE	24
 gtexture_color:
-	.space	24
+	.SPACE	24
 gsolver_w_vec:
-	.space	24
+	.SPACE	24
 gchkinside_p:
-	.space	24
+	.SPACE	24
 gisoutside_q:
-	.space	24
+	.SPACE	24
 gnvector_w:
-	.space	24
+	.SPACE	24
 gscan_d:
-	.space	8
+	.SPACE	8
 gscan_offset:
-	.space	8
+	.SPACE	8
 gscan_sscany:
-	.space	8
+	.SPACE	8
 gscan_met1:
-	.space	8
+	.SPACE	8
 gwscan:
-	.space	24
+	.SPACE	24
