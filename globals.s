d7271361823dfd48d6b88f632d1ffb04760871a9
Tran Sang <tranvansangk41@gmail.com>
2016-11-29 16:08:15 +0900

    fix global.s

--- globals.s
+++ globals.s
@@ -1,69 +1,69 @@
 .data
-gobjects:
-	.SPACE	240
-gsize:
-	.SPACE	8
-gdbg:
-	.SPACE	4
-gscreen:
-	.SPACE	24
-gvp:
-	.SPACE	24
-gview:
-	.SPACE	24
-glight:
-	.SPACE	24
-gcos_v:
-	.SPACE	16
-gsin_v:
-	.SPACE	16
-gbeam:
-	.SPACE	8
-gand_net:
-	.SPACE	200
-gor_net:
-	.SPACE	4
-gtemp:
-	.SPACE	112
-gcs_temp:
-	.SPACE	128
-gsolver_dist:
-	.SPACE	8
-gvscan:
-	.SPACE	24
-gintsec_rectside:
-	.SPACE	4
-gtmin:
-	.SPACE	8
-gcrashed_point:
-	.SPACE	24
-gcrashed_object:
-	.SPACE	4
-gend_flag:
-	.SPACE	4
-gviewpoint:
-	.SPACE	24
-gnvector:
-	.SPACE	24
-grgb:
-	.SPACE	24
-gtexture_color:
-	.SPACE	24
-gsolver_w_vec:
-	.SPACE	24
-gchkinside_p:
-	.SPACE	24
-gisoutside_q:
-	.SPACE	24
-gnvector_w:
-	.SPACE	24
-gscan_d:
-	.SPACE	8
-gscan_offset:
-	.SPACE	8
-gscan_sscany:
-	.SPACE	8
-gscan_met1:
-	.SPACE	8
-gwscan:
-	.SPACE	24
+min_caml_objects:
+	.SPACE 240
+min_caml_size:
+	.SPACE 8
+min_caml_dbg:
+	.SPACE 4
+min_caml_screen:
+	.SPACE 24
+min_caml_vp:
+	.SPACE 24
+min_caml_view:
+	.SPACE 24
+min_caml_light:
+	.SPACE 24
+min_caml_cos_v:
+	.SPACE 16
+min_caml_sin_v:
+	.SPACE 16
+min_caml_beam:
+	.SPACE 8
+min_caml_and_net:
+	.SPACE 200
+min_caml_or_net:
+	.SPACE 4
+min_caml_temp:
+	.SPACE 112
+min_caml_cs_temp:
+	.SPACE 128
+min_caml_solver_dist:
+	.SPACE 8
+min_caml_vscan:
+	.SPACE 24
+min_caml_intsec_rectside:
+	.SPACE 4
+min_caml_tmin:
+	.SPACE 8
+min_caml_crashed_point:
+	.SPACE 24
+min_caml_crashed_object:
+	.SPACE 4
+min_caml_end_flag:
+	.SPACE 4
+min_caml_viewpoint:
+	.SPACE 24
+min_caml_nvector:
+	.SPACE 24
+min_caml_rgb:
+	.SPACE 24
+min_caml_texture_color:
+	.SPACE 24
+min_caml_solver_w_vec:
+	.SPACE 24
+min_caml_chkinside_p:
+	.SPACE 24
+min_caml_isoutside_q:
+	.SPACE 24
+min_caml_nvector_w:
+	.SPACE 24
+min_caml_scan_d:
+	.SPACE 8
+min_caml_scan_offset:
+	.SPACE 8
+min_caml_scan_sscany:
+	.SPACE 8
+min_caml_scan_met1:
+	.SPACE 8
+min_caml_wscan:
+	.SPACE 24
