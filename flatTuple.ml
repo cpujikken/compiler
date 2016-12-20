24a52f9377bd114f5270b59314953897533b29e0
Tran Sang <tranvansangk41@gmail.com>
2016-12-20 10:03:11 +0900

    fix flattuple.ml

--- flatTuple.ml
+++ flatTuple.ml
@@ -17,22 +17,52 @@ let rec flat_type = function
   | Type.Var({contents = Some(t)} as r, info) ->
           r := Some(flat_type t);
           Type.Var(r, info)
-    | other -> other
+  | Type.Var ({contents = None}, info) ->
+          Type.Var(ref None, info)
   (*| Type.Var (ref_opt_t, info) -> Type.Var*)
 
-let rec flat = function
+  (*cases:
+      * lettuple a, b = (x, (y, z))
+      * --> lettuple t1, t2, t3  =  x, y, z in let a = t1 in let b = t2, t3
+      * let x = (u, (v, w), t)
+      * type process
+      *
+      * tuple(a, b, c) with a = int(1), b = tuple(3, 4.0), c = float(1.) defined before, but unknown yet.
+      * Solution: attach type enviroment for only tuple type -> only in Let expression
+      * there no more tuple deinfed in lettuple expression (all are already flatten)
+      * Mapping tuple type variable to its elements' type after expanded
+      * Ex: b->[Type.int, Type.float]
+      * Then:
+          * let tuple (b1:Int, b2:Float) = b in tuple (a, b1, b2, c)
+      *)
+
+let rec flat env = function
 
   | Let ((var, var_type), value, body, info)
-    -> Let((var, flat_type var_type), flat value, flat body, info)
+    ->
+        let new_type = flat_type var_type
+        in
+        let new_exp = flat env value
+        in
+        let env = match new_type with
+            | Type.Tuple (tlist, _) -> M.add var tlist env
+            | _ -> env
+        in
+            Let((var, new_type), new_exp, flat env body, info)
 
   | MakeCls ((id, typ), cls, exp, info)
-    -> MakeCls((id, flat_type typ), cls, flat exp, info)
+    -> MakeCls((id, flat_type typ), cls, flat env exp, info)
 
+    (*consider id is already expaned*)
+    (*note: a lot of Let expressions appear after wrap_after_flat.
+     * They are already expanded, but there is need to flat them to update env*)
   | LetTuple (id_type_list, id, exp, info)
     ->
         let id_type_list', replacements = flat_id_type_list id_type_list
         in
-        let new_exp = wrap_after_flat replacements (flat exp)
+        let wrapped_exp = wrap_after_flat replacements exp
+        in
+        let new_exp = flat env wrapped_exp
         in
         (*reassign flatten variable*)
         LetTuple(
@@ -42,10 +72,64 @@ let rec flat = function
             info
         )
   | IfEq (id1, id2, e1, e2, info)
-    -> IfEq(id1, id2, flat e1, flat e2, info)
+    -> IfEq(id1, id2, flat env e1, flat env e2, info)
   | IfLE (id1, id2, e1, e2, info)
-  -> IfLE(id1, id2, flat e1, flat e2, info)
-    | other-> other
+  -> IfLE(id1, id2, flat env e1, flat env e2, info)
+    | Tuple(idlist, info)
+    ->
+      let new_id_types, replacements = List.fold_left (fun (id_list, replacements) id ->
+          try
+              let types_list = M.find id env
+              in
+              let new_id_types =
+                  List.fold_right (fun typ current -> (Id.genid id, typ) :: current) types_list []
+              in
+              let new_ids = List.map fst new_id_types
+              in
+                id_list @ new_ids, replacements @ [new_id_types, id]
+          with Not_found -> id_list @ [id], replacements
+      )
+      ([], [])
+      idlist
+      in
+          List.fold_right (fun (new_id_types, id) exp ->
+              LetTuple(new_id_types, id, exp, info)
+          )
+          replacements
+          (Tuple(new_id_types, info))
+
+    | Unit _
+    | IntRead _
+    | FloatRead _
+    | Int _
+    | Float _
+    | Neg _
+    | Print _
+    | Four _
+    | Half _
+    | Add _
+    | ShiftLeft _
+    | ShiftRight _
+    | Div _
+    | Mul _
+    | Sub _
+    | FNeg _
+    | FAbs _
+    | Array _
+    | FAdd _
+    | FSub _
+    | FMul _
+    | FDiv _
+    | Var _
+
+    | AppCls _
+    | AppDir _
+    | Get _
+    | Put _
+    | ExtArray _
+    as exp
+    ->
+        exp
 and flat_id_type_list id_type_list =
     List.fold_right
         (fun (id, typ) (tuple_list, replacements) -> match flat_type typ with
@@ -71,7 +155,7 @@ and wrap_after_flat replacements exp =
 let fun_converter { name = (fun_name , fun_type); args = args_id_types; formal_fv = free_args_id_types; body = body; info = info } =
     let new_args_id_types, replacements = flat_id_type_list args_id_types
     in
-    let new_body = wrap_after_flat replacements (flat body)
+    let new_body = wrap_after_flat replacements (flat M.empty body)
     in
     let new_free_args_id_types, replacements_fv = flat_id_type_list free_args_id_types
     in
@@ -89,7 +173,7 @@ let fun_converter { name = (fun_name , fun_type); args = args_id_types; formal_f
 
 let f (Prog(fundefs, e)) =
 (*Printf.printf "Before flatten\n%s\n" (to_string e);*)
-    let new_e = flat e
+    let new_e = flat M.empty e
     in
     (*Printf.printf "After flatten\n%s\n" (to_string new_e);*)
     Prog(List.map fun_converter fundefs, new_e)
